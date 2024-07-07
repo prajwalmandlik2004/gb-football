6b6b28110286940bc1f8110747c132d5913093be
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2024-07-07 15:48:31 +0530

    new update

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.CONNECTION_MONGO)
+mongoose.connect(process.env.CONNECTION_MONGO, { autoIndex: false })
     .then(() => console.log("Connected Successfully !!"))
     .catch((error) => console.log(error));
 
